9d158f9eba63b6c4f3f684a81ada4b3b1803513c
Tona Morales-Calkins <tona.mc@gmail.com>
2018-10-04 00:32:05 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -6,6 +6,10 @@ function updatePlaylist(playlist, artistName, songTitle) {
 }
 
 function removeFromPlaylist(playlist, artistName) {
+
   delete playlist[artistName];
+
+  delete playlist.artistName;
+
   return(playlist);
 }
